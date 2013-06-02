4c3c088891255d1e0c3439ec2b904ca51bd8ec07
govindragh <govindragh@gmail.com>
2013-06-01 19:13:20 -0700

    First code commit

---
+++ DBInterface.py
@@ -0,0 +1,98 @@
+import sys
+import sqlite3
+
+################################################################################
+# HELPER FUNCTIONS
+################################################################################
+
+def printoptions():
+    print 'Select one of the following options:'
+    print '1.) List all targets.'
+    print '2.) Add a new target.'
+    print '3.) Delete a target.'
+    print '4.) List all accumulated data for a target.'
+    print '5.) Find all links to the target.'
+
+def selectionprompt():
+    inp = raw_input(' > ')
+    try:
+        ret = int(inp)
+        return ret
+    except ValueError:
+        return -1
+
+def listtargets(dbconn, dbcur):
+    dbcur.execute('SELECT Target FROM Targets')
+    targetrows = dbcur.fetchall()
+    for targetrow in targetrows:
+        print targetrow[0]
+
+def addtarget(dbconn, dbcur):
+    print '1.) Input the target manually.'
+    print '2.) Import a text file of targets.'
+    selection = selectionprompt()
+    if selection == 1:
+        newtarget = raw_input('Please enter the new target (enter nothing to cancel): ')
+        if newtarget != '':
+            dbcur.execute('INSERT INTO Targets (Target) VALUES (?)', (newtarget,))
+            dbconn.commit()
+    elif selection == 2:
+        filepath = raw_input('Please enter the file path (enter nothing to cancel): ')
+        if filepath != '':
+            targetsfile = open(filepath, 'r')
+            targetstext = targetsfile.read()
+            targets = targetstext.split('\n')
+            for newtarget in targets:
+                dbcur.execute('INSERT INTO Targets (Target) VALUES (?)', (newtarget,))
+                dbconn.commit()
+
+def deltarget(dbconn, dbcur):
+    deltarget = raw_input('Please enter the target to delete (enter nothing to cancel): ')
+    if deltarget != '':
+        dbcur.execute('DELETE FROM Targets WHERE Target = ?', (deltarget,))
+        dbconn.commit()
+
+def listdata(dbconn, dbcur):
+    listtarget = raw_input('Please enter the target to list the data for (enter nothing to cancel): ')
+    if listtarget != '':
+        dbcur.execute('SELECT IPAddress, FirstDetected, LastDetected FROM Addresses WHERE DomainName = ?', (listtarget,))
+        addressrows = dbcur.fetchall()
+        for addressrow in addressrows:
+            print str(addressrow)
+        dbcur.execute('SELECT Addresses.DomainName, Type, MXPref, IPAddress, AssociatedServers.FirstDetected, AssociatedServers.LastDetected, Addresses.FirstDetected, Addresses.LastDetected FROM AssociatedServers, Addresses WHERE ParentDomain = ? AND AssociatedServers.DomainName = Addresses.DomainName ORDER BY Type, Addresses.DomainName', (listtarget,))
+        addressrows = dbcur.fetchall()
+        for addressrow in addressrows:
+            print str(addressrow)
+
+def findlinks(dbconn, dbcur):
+    print 'Not implemented yet.'
+
+################################################################################
+# MAIN
+################################################################################
+
+if len(sys.argv) != 2:
+    print 'Usage: DomainTracker.py <Database filepath>'
+    exit()
+
+dbfilepath = sys.argv[1]
+
+dbconn = sqlite3.connect(dbfilepath)
+dbcur = dbconn.cursor()
+while True:
+    printoptions()
+    selection = selectionprompt()
+    print ''
+    if selection == 1:
+        listtargets(dbconn, dbcur)
+    elif selection == 2:
+        addtarget(dbconn, dbcur)
+    elif selection == 3:
+        deltarget(dbconn, dbcur)
+    elif selection == 4:
+        listdata(dbconn, dbcur)
+    elif selection == 5:
+        findlinks(dbconn, dbcur)
+    else:
+        print 'Please select a valid option!'
+    print '================================================================================'
---
+++ DomainTracker.py
@@ -0,0 +1,88 @@
+import sys
+import os
+import sqlite3
+import subprocess
+import re
+import datetime
+
+################################################################################
+# HELPER FUNCTIONS
+################################################################################
+
+def initdb(dbfilepath):
+    dbconn = sqlite3.connect(dbfilepath)
+    dbcur = dbconn.cursor()
+    dbcur.execute('CREATE TABLE Targets(Target TEXT)')
+    dbcur.execute('CREATE TABLE AssociatedServers(ParentDomain TEXT, DomainName TEXT, Type TEXT, MXPref INT, FirstDetected TEXT, LastDetected TEXT)')
+    dbcur.execute('CREATE TABLE Addresses(DomainName TEXT, IPAddress TEXT, FirstDetected TEXT, LastDetected TEXT)')
+    dbconn.commit()
+    dbconn.close()
+
+def inetaddr(lineregex, dbcur):
+    DomainName = lineregex.group(1)
+    IPAddress = lineregex.group(3)
+    dbcur.execute('SELECT * FROM Addresses WHERE DomainName = ? AND IPAddress = ?', (DomainName, IPAddress))
+    existaddr = dbcur.fetchall()
+    currtime = datetime.datetime.now()
+    if len(existaddr) > 0:
+        dbcur.execute('UPDATE Addresses SET LastDetected = ? WHERE DomainName = ? AND IPAddress = ?', (currtime, DomainName, IPAddress))
+    else:
+        dbcur.execute('INSERT INTO Addresses (DomainName, IPAddress, FirstDetected, LastDetected) VALUES (?, ?, ?, ?)', (DomainName, IPAddress, currtime, currtime))
+
+def assocserv(lineregex, dbcur):
+    ParentDomain = target
+    if lineregex.group(2) == 'nameserver':
+        Type = 'NameServer'
+        MXPref = 0
+    else:
+        Type = 'MailExchanger'
+        mxregex = re.search('MX preference = (\d*), mail exchanger', lineregex.group(2), re.DOTALL)
+        MXPref = mxregex.group(1)
+    DomainName = lineregex.group(3)
+    dbcur.execute('SELECT * FROM AssociatedServers WHERE ParentDomain = ? AND Type = ? AND DomainName = ?', (ParentDomain, Type, DomainName))
+    existassoc = dbcur.fetchall()
+    currtime = datetime.datetime.now()
+    if len(existassoc) > 0:
+        dbcur.execute('UPDATE AssociatedServers SET LastDetected = ? WHERE ParentDomain = ? AND Type = ? AND DomainName = ?', (currtime, ParentDomain, Type, DomainName))
+    else:
+        dbcur.execute('INSERT INTO AssociatedServers (ParentDomain, Type, DomainName, FirstDetected, LastDetected) VALUES (?, ?, ?, ?, ?)', (ParentDomain, Type, DomainName, currtime, currtime))
+
+def processline(line, dbcur):
+    lineregex = re.search('(.*?)\s*(internet address|nameserver|MX preference = \d*, mail exchanger) = (.*)\s', line, re.DOTALL)
+    if lineregex != None:
+        if lineregex.group(2) == 'internet address':
+            inetaddr(lineregex, dbcur)
+        else:
+            assocserv(lineregex, dbcur)
+
+################################################################################
+# MAIN
+################################################################################
+
+if len(sys.argv) != 2:
+    print 'Usage: DomainTracker.py <Database filepath>'
+    exit()
+
+dbfilepath = sys.argv[1]
+
+try:
+    dbfile = open(dbfilepath, 'r')
+    dbfile.close()
+except IOError:
+    dbfile = open(dbfilepath, 'w')
+    dbfile.close()
+    initdb(dbfilepath)
+
+dbconn = sqlite3.connect(dbfilepath)
+dbcur = dbconn.cursor()
+
+while True:
+    dbcur.execute('SELECT Target FROM Targets')
+    targetrows = dbcur.fetchall()
+    for targetrow in targetrows:
+        target = targetrow[0]
+        output = subprocess.check_output(['nslookup', '-query=all', target])
+        outlines = output.split('\n')
+        for line in outlines:
+            processline(line, dbcur)
+        dbconn.commit()
